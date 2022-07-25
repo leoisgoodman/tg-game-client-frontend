8d5fa3c660584995d4faf604a311e9c7d5bc77c2
PeterCode998 <peter@cg.support>
2022-07-25 19:36:10 +0800

    图片不用hash放入版本文件夹

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,21 @@ const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 module.exports = defineConfig({
   transpileDependencies: true,
-  filenameHashing: false,
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
       patterns: [path.resolve(__dirname, './src/style/global.less')],
     },
+  },
+  // 图片不用hash放入版本文件夹
+  chainWebpack: (config) => {
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|webp|avif)(\?.*)?$/)
+      .set('type', 'asset')
+      .set('generator', {
+        filename: `1.0.2/[name][ext]`,
+      })
+      .end();
   },
 });
